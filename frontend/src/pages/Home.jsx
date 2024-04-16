00b717861abe4389beb9c9d9516a4b7fb446342f
jhhocs <joshuaho1@cpp.edu>
2024-04-16 01:03:09 -0700

    more styling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,94 +1,170 @@
+import { useEffect } from "react";
 import { useForm, useFieldArray } from "react-hook-form";
 
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Unstable_Grid2';
-import InputBase from '@mui/material/InputBase';
-import Paper from '@mui/material/Paper';
-import TextField from '@mui/material/TextField';
+import { InputAdornment } from "@mui/material";
 
-import AddIcon from '@mui/icons-material/Add';
-import ClearIcon from '@mui/icons-material/Clear';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Unstable_Grid2";
+import InputBase from "@mui/material/InputBase";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
+
+import AddIcon from "@mui/icons-material/Add";
+import ClearIcon from "@mui/icons-material/Clear";
 
 function Home() {
-    const { register, handleSubmit } = useForm();
-    const { register: register2, handleSubmit: handleSubmit2, control } = useForm();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState,
+    formState: { errors, isSubmitSuccessful },
+    getValues,
+  } = useForm({ defaultValues: { ingridient: "" } });
+  const {
+    register: register2,
+    handleSubmit: handleSubmit2,
+    control,
+  } = useForm();
 
-    const { fields, append, remove } = useFieldArray({
-        control,
-        name: "ingridients"
-    });
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "ingridients",
+  });
 
-    const onSubmitIngridient = (data) => {
-        append(data.ingridient);
-        console.log(data)
+  const onSubmitIngridient = (data) => {
+    append(data.ingridient);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    if (isSubmitSuccessful && !errors.ingridient) {
+      console.log("reset");
+      reset({ ingridient: "" });
     }
+  }, [formState, reset]);
 
-    const onSubmit = (data) => {
-        console.log(data);
-    };
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
-    return (
-        <Grid 
-            container
-            alignItems="center" 
-            justifyContent="center"
-            // my={4} 
-            sx={{ minHeight: '100vh' }}>
-            <Grid 
-                item xs={3} 
-                container spacing={2} 
-                className = "Home" 
-                border={1} 
-                padding={5} 
-                borderRadius={2}
-                display={"flex"}
-                wrap="nowrap"
+  return (
+    <Grid
+      container
+      alignItems="center"
+      justifyContent="center"
+      // my={4}
+      //   sx={{ marginTop: "20vh" }}
+    >
+      <Grid
+        border={1}
+        padding={5}
+        borderRadius={2}
+        margin={10}
+        sx={{ width: "50vw", minHeight: "60vh" }}
+      >
+        <Grid
+          item
+          xs={3}
+          container
+          spacing={2}
+          className="Home"
+          //   display={"flex"}
+          //   wrap="wrap"
+          direction="row"
+          sx={
+            {
+              // minHeight: '40vh',
+              // minWidth: '80vh',
+              // height:'50vh',
+              // width:'75vh'
+            }
+          }
+        >
+          <Grid sx={{ width: "40%" }}>
+            <h2>Recipe Generator</h2>
+            <Paper
+              key={1}
+              component="form"
+              sx={{
+                p: "2px 4px",
+                display: "flex",
+                alignItems: "center",
+                // width: "10vw",
+              }}
+              onSubmit={handleSubmit(onSubmitIngridient)}
+            >
+              <InputBase
+                variant="outlined"
+                type="text"
+                placeholder="Enter ingridient"
+                sx={{ ml: 1, flex: 1 }}
+                {...register("ingridient", {
+                  validate: (value) => {
+                    if (value.replace(/\s/g, "") === "") {
+                      return false;
+                    }
+                    return true;
+                  },
+                })}
+              />
+              <Button
+                variant="outlined"
+                type="submit"
+                sx={{ minWidth: "20px", padding: "0px" }}
+              >
+                <AddIcon />
+              </Button>
+            </Paper>
+          </Grid>
+          <Grid sx={{ width: "60%" }}>
+            <p>Ingrediants</p>
+            <form id="form" key={2} onSubmit={handleSubmit2(onSubmit)}>
+              <Box
+                pacing={{ xs: 1, sm: 2 }}
                 direction="row"
-                sx={{
-                    minHeight: '40vh',
-                    minWidth: '80vh',
-                    // height:'50vh',
-                    // width:'75vh'
-                }}>
-                <Grid alignSelf="center" >
-                    <Paper 
-                        key = {1} 
-                        component="form"
-                        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', minWidth: '30vh' }}
-                        onSubmit = {handleSubmit(onSubmitIngridient)}>
-                        <InputBase 
-                            variant="outlined" 
-                            type="text" 
-                            placeholder="Enter ingridient" 
-                            sx={{ ml: 1, flex: 1 }}
-                            {...register("ingridient")} />
-                        <Button 
-                            variant="outlined" 
-                            type="submit" 
-                            sx={{ minWidth:'20px', padding:'0px'}}>
-                            <AddIcon />
-                        </Button>
-                    </Paper>
-                </Grid>
-                <Grid>
-                    <form key = {2} onSubmit = { handleSubmit2(onSubmit)}>
-                        <ul>  {/*https://mui.com/material-ui/react-grid/   https://mui.com/material-ui/react-grid2/*/}
-                            {fields.map((item, index) => (
-                            <li key={item.id}>
-                                <TextField variant="outlined" type="text" placeholder="Ingredient" readOnly {...register2(`ingridients.${index}`)} />
-                                <Button variant="outlined" type="button" onClick={() => remove(index)}><ClearIcon /></Button>
-                            </li>
-                            ))}
-                            <TextField variant="outlined" type="text" placeholder="Cuisine" {...register2("cuisine")} />
-                        </ul>
-
-                        <Button variant="outlined" type="submit" >Submit</Button>
-                    </form>
-                </Grid>
-            </Grid>
+                useFlexGap
+                flexWrap="wrap"
+              >
+                {fields.map((item, index) => (
+                  <TextField
+                    key={item.id}
+                    variant="outlined"
+                    type="text"
+                    placeholder="Ingredient"
+                    fullWidth
+                    style={{ minWidth: "100px", width: "100px" }}
+                    InputProps={{
+                      readOnly: true,
+                      endAdornment: (
+                        <InputAdornment position="start">
+                          <ClearIcon />
+                        </InputAdornment>
+                      ),
+                    }}
+                    {...register2(`ingridients.${index}`)}
+                    onClick={() => remove(index)}
+                  />
+                ))}
+                <TextField
+                  variant="outlined"
+                  type="text"
+                  placeholder="Cuisine"
+                  {...register2("cuisine")}
+                />
+              </Box>
+            </form>
+          </Grid>
         </Grid>
-    )
+        <Button form="form" variant="outlined" type="submit">
+          Submit
+        </Button>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Home;

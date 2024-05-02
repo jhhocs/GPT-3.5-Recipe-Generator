1f9fbfcee4f891d4a6be88fe4b5a2e214e488b46
jhhocs <joshuaho1@cpp.edu>
2024-05-02 12:13:31 -0700

    Cleaned up Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,8 +9,6 @@ import Button from "@mui/material/Button";
 import LoadingButton from "@mui/lab/LoadingButton";
 import Grid from "@mui/material/Unstable_Grid2";
 import InputBase from "@mui/material/InputBase";
-// import List from "@mui/material/List";
-// import ListItem from "@mui/material/ListItem";
 import Paper from "@mui/material/Paper";
 import TextField from "@mui/material/TextField";
 
@@ -31,13 +29,11 @@ function Home() {
     reset,
     formState,
     formState: { errors, isSubmitSuccessful },
-    // getValues,
   } = useForm({ defaultValues: { ingridient: "" } });
 
   const {
     register: register2,
     handleSubmit: handleSubmit2,
-    // getValues,
     control,
   } = useForm();
 
@@ -49,16 +45,8 @@ function Home() {
   const navigate = useNavigate();
 
   const onSubmitIngridient = (data) => {
-    // if (data.ingridient.length * 10 > 50) {
     setInputWidth([...inputWidth, data.ingridient.length * 8 + 50 + "px"]);
-
-    // data.inputWidth = data.ingridient.length * 10 + "px";
-    // } else {
-    // data.inputWidth = 50;
-    // }
     append(data.ingridient);
-    // console.log(getValues(`ingredients.${0}.inputWidth`));
-    // console.log(getValues(`ingredients.${0}`));
   };
 
   const callAPI = async (data) => {
@@ -68,70 +56,22 @@ function Home() {
       data: {
         cuisine: data.cuisine,
         ingredients: data.ingredients.toString(),
-        // cuisine: "Italian",
       },
     })
       .then((response) => {
         console.log(response.data);
-        // setResponse(JSON.parse(result.choices[0].message.content));
         setLoading(false);
-        // this.props.history.push("/recepies/" + response.data.id);
         navigate("/recipes/" + response.data.id);
       })
       .catch((error) => {
         console.log(error);
         setLoading(false);
-        // setTimeout(() => {
         setError(true);
-        // }, 3000);
         setTimeout(() => {
           setError(false);
         }, 3000);
       });
   };
-
-  // function Response() {
-  //   if (!hasResponse) {
-  //     return;
-  //   } else {
-  //     return (
-  //       <div>success</div>
-  //       // <Redirect to={"/recepies/" + response.id} />
-  //       // <div>
-  //       //   {
-  //       //     <div>
-  //       //       <h1>{response.name}</h1>
-  //       //       <h4>{"Difficulty: " + response.difficulty}</h4>
-  //       //       <p>{"Preparation Time: " + response.prep_time}</p>
-  //       //       <p>{"Cook Time: " + response.cook_time}</p>
-  //       //       <p>{"Total Time: " + response.total_time}</p>
-  //       //       <p>{"Servings: " + response.servings}</p>
-  //       //       <h2>Ingredients</h2>
-  //       //       <ul>
-  //       //         {response.ingredients.map((ingredient, index) => (
-  //       //           <li key={index}>
-  //       //             {ingredient.name + " " + ingredient.quantity}
-  //       //           </li>
-  //       //         ))}
-  //       //       </ul>
-  //       //       <h2>Instructions</h2>
-  //       //       <ol>
-  //       //         {response.instructions.map((instruction, index) => (
-  //       //           <li key={index}>{instruction}</li>
-  //       //         ))}
-  //       //       </ol>
-  //       //       <h2>Additional Notes</h2>
-  //       //       <ul>
-  //       //         {response.notes.map((note, index) => (
-  //       //           <li key={index}>{note}</li>
-  //       //         ))}
-  //       //       </ul>
-  //       //     </div>
-  //       //   }
-  //       // </div>
-  //     );
-  //   }
-  // }
 
   useEffect(() => {
     if (isSubmitSuccessful && !errors.ingridient) {
@@ -150,14 +90,7 @@ function Home() {
 
   return (
     <div>
-      {/* <Navbar /> */}
-      <Grid
-        container
-        alignItems="center"
-        justifyContent="center"
-        // my={4}
-        //   sx={{ marginTop: "20vh" }}
-      >
+      <Grid container alignItems="center" justifyContent="center">
         <Grid
           border={1}
           padding={5}
@@ -171,17 +104,7 @@ function Home() {
             container
             spacing={2}
             className="Home"
-            //   display={"flex"}
-            //   wrap="wrap"
             direction="row"
-            sx={
-              {
-                // minHeight: '40vh',
-                // minWidth: '80vh',
-                // height:'50vh',
-                // width:'75vh'
-              }
-            }
           >
             <Grid sx={{ width: "40%" }}>
               <h2>Recipe Generator</h2>
@@ -192,7 +115,6 @@ function Home() {
                   p: "2px 4px",
                   display: "flex",
                   alignItems: "center",
-                  // width: "10vw",
                 }}
                 onSubmit={handleSubmit(onSubmitIngridient)}
               >
@@ -201,7 +123,6 @@ function Home() {
                   type="text"
                   placeholder="Enter ingridient"
                   autoComplete="off"
-                  // margin={1}
                   sx={{ ml: 1, flex: 1 }}
                   {...register("ingridient", {
                     validate: (value) => {
@@ -239,7 +160,6 @@ function Home() {
               </Box>
               <form id="form" key={2} onSubmit={handleSubmit2(onSubmit)}>
                 <Box
-                  // pacing={{ xs: 1, sm: 2 }}
                   direction="row"
                   useFlexGap
                   flexWrap="wrap"
@@ -255,14 +175,10 @@ function Home() {
                         placeholder="Ingredient"
                         size="small"
                         sx={{
-                          // minWidth: "100px",
-                          // width: "80px",
                           width: inputWidth[index] ? inputWidth[index] : "50px",
                           marginLeft: "5px",
                           marginBottom: "5px",
                           input: { cursor: "pointer" },
-                          // margin: "10px",
-                          // height: "10px",
                         }}
                         InputProps={{
                           readOnly: true,
@@ -270,8 +186,6 @@ function Home() {
                             <InputAdornment
                               position="start"
                               sx={{
-                                // width: "100%",
-                                // height: "100%",
                                 marginRight: "-5px",
                               }}
                             >
